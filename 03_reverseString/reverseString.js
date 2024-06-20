d3d38d9b3e2b120502b5b66f19788709962a1dea
hazharmaroufi <hazhmar@gmail.com>
2024-06-20 18:56:15 +0330

    Pass exc 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
 
+
+
+const reverseString = function (str) {
+  let strLength = str.length;
+  let reversedStr = "";
+  let strArr = [];
+  for (let i = strLength - 1; i >= 0; i--) {
+    strArr.push(str[i])
+  }
+  for (let i = 0; i < strLength; i++) {
+    reversedStr += strArr[i];
+  }
+  return reversedStr;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
