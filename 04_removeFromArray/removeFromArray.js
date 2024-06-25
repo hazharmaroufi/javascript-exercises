3f49df64135c7a7201ae767da799b8ec9eb4dbb7
hazharmaroufi <hazhmar@gmail.com>
2024-06-25 13:45:36 +0330

    Done ex-4 with chitting in codes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+//const args = [...arguments];
+//function f(a, b, ...theArgs)
+
+const removeFromArray = function (arr, ...str) {
+  for (let i = 0; i < str.length; i++) {
+    let index = arr.indexOf(str[i]);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    index = arr.indexOf(str[i]);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
