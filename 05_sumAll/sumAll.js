845fbc6d332d01820e73e23a44714637be834b8a
hazharmaroufi <hazhmar@gmail.com>
2024-06-25 13:59:35 +0330

    Pass the 05 ; i liked that

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (first, last) {
+  let sum = 0;
+  if (first < 0 || last < 0 || typeof first !== 'number' || typeof last !== 'number') {
+    return "ERROR";
+  }
+  if (last < first) {
+    let tempNum = first;
+    first = last;
+    last = tempNum;
+  }
+  for (let i = first; i <= last; i++) {
+    sum += Number(i);
+  }
+  return sum;
 };
 
 // Do not edit below this line
