fe4e6aec0b4578ceb0512ee8babb55a19c145777
hazharmaroufi <hazhmar@gmail.com>
2024-07-08 01:00:27 +0330

    The 9 in completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (a) {
+  a1 = a.replace('.', '');
+  a2 = a1.replace('!', '');
+  a3 = a2.replace(',', '');
+  a4 = a3.replace(',', '');
+  a5 = a4.toLowerCase().split(" ").join("");
 
+  let nString = [];
+  for (let i = a5.length; i >= 0; i--) {
+    nString.push(a5[i]);
+  }
+  nString2 = nString.join("").toLowerCase().split(" ").join("");
+
+
+  if (nString2 === a5) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
