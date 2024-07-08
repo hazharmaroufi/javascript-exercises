39d7bd89c822e646b3b30007b076623de96efb9f
hazharmaroufi <hazhmar@gmail.com>
2024-07-08 18:31:53 +0330

    I solved the fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  if (a < 0) {
+    return "OOPS";
+  }
+  let fib = [0, 1];
+  for (let i = 1; i <= a; i++) {
+    fib.push(fib[i] + fib[i - 1]);
+  }
+  return (fib[a]);
 };
 
 // Do not edit below this line
