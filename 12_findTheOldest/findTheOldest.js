dcd378bc2aaf9716d3eafca98ccb186ea9bef78c
hazharmaroufi <hazhmar@gmail.com>
2024-07-08 19:28:43 +0330

    solved this one

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (person) {
+  let year = new Date().getFullYear();
 
-};
-
+  let sorted = person.sort((a, b) =>
+    ((isNaN(a.yearOfDeath) ? year : a.yearOfDeath)
+      - a.yearOfBirth)
+    - ((isNaN(b.yearOfDeath) ? year : b.yearOfDeath)
+      - b.yearOfBirth)
+  );
+  return sorted[person.length - 1];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
